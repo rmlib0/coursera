1c4a3e36a429c5f301f35694117fa6db29caf64b
Mikhail Ovsyannikov (rmlib.null) <72548588+rmlib0@users.noreply.github.com>
2020-11-06 22:45:12 +0300

    Update rock-paper-scissors-lizard-spock.py

--- rock-paper-scissors-lizard-spock.py
+++ rock-paper-scissors-lizard-spock.py
@@ -1,5 +1,5 @@
 # Rock-paper-scissors-lizard-Spock
-# http://www.codeskulptor.org/#user47_KLRTDWmOaE0uJEM.py
+# http://www.codeskulptor.org/#user47_YOLUvuCncZXQyji.py
 import random
 
 
@@ -20,8 +20,8 @@ def name_to_number(name):
 
 
 def game_winner(player_choice, computer_choice, who):
-    print(f"Player chooses: {player_choice}")
-    print(f"Computer chooses: {computer_choice}")
+    print(f"Player chooses: {number_to_name(player_choice)}")
+    print(f"Computer chooses: {number_to_name(computer_choice)}")
     if who == "player":
         print(f"Player wins!")
     elif who == "computer":
@@ -31,35 +31,21 @@ def game_winner(player_choice, computer_choice, who):
 
 
 def rpsls(_player_choice):
-    computer_choice = number_to_name(random.randrange(0, 5, 1))
-    player_choice = _player_choice
+    computer_choice = random.randint(0, 4)
+    player_choice = name_to_number(_player_choice)
 
-    if computer_choice == player_choice:
+    difference = (computer_choice - player_choice) % 5
+
+    if difference == 1 or difference == 2:
+        game_winner(player_choice, computer_choice, "computer")
+    elif difference == 4 or difference == 3:
+        game_winner(player_choice, computer_choice, "player")
+    elif difference == 0:
         game_winner(player_choice, computer_choice, "draw")
-    elif player_choice == "rock":
-        if computer_choice == "lizard" or computer_choice == "scissors":
-            game_winner(player_choice, computer_choice, "player")
-        else:
-            game_winner(player_choice, computer_choice, "computer")
-    elif player_choice == "Spock":
-        if computer_choice == "rock" or computer_choice == "scissors":
-            game_winner(player_choice, computer_choice, "player")
-        else:
-            game_winner(player_choice, computer_choice, "computer")
-    elif player_choice == "paper":
-        if computer_choice == "rock" or computer_choice == "Spock":
-            game_winner(player_choice, computer_choice, "player")
-        else:
-            game_winner(player_choice, computer_choice, "computer")
-    elif player_choice == "lizard":
-        if computer_choice == "paper" or computer_choice == "Spock":
-            game_winner(player_choice, computer_choice, "player")
-        else:
-            game_winner(player_choice, computer_choice, "computer")
-    elif player_choice == "scissors":
-        if computer_choice == "paper" or computer_choice == "lizard":
-            game_winner(player_choice, computer_choice, "player")
-        else:
-            game_winner(player_choice, computer_choice, "computer")
-    else:
-        print("no way!")
+
+
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
